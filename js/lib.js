08a049ceebbbac5b11fc054ec94702cfd08c5d07
edom18 <edo.m18@gmail.com>
2015-11-27 14:25:34 +0900

    Add a new function.

--- js/lib.js
+++ js/lib.js
@@ -129,6 +129,31 @@
         return texture;
     }
 
+    function createFramebuffer(gl, width, height) {
+        var framebuffer = gl.createFramebuffer();
+
+        gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer);
+
+        // var depthBuffer = gl.createRenderbuffer();
+        // gl.bindRenderbuffer(gl.RENDERBUFFER, depthBuffer);
+        // gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, width, height);
+        // gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthBuffer);
+        var texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
+
+        gl.bindTexture(gl.TEXTURE_2D, null);
+        gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+        return framebuffer;
+    }
+
     // Exports
     ns.compileShader = compileShader;
     ns.createShader  = createShader;
@@ -136,5 +161,6 @@
     ns.createVBO     = createVBO;
     ns.createIBO     = createIBO;
     ns.createTexture = createTexture;
+    ns.createFramebuffer = createFramebuffer;
 
 }(window));
