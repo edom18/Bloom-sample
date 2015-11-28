9fa0b0d51d7e1388250bbd2487ef56fbd67de668
edom18 <edo.m18@gmail.com>
2015-11-29 08:50:14 +0900

    Implemente several shader control

--- js/main.js
+++ js/main.js
@@ -4,15 +4,23 @@
     var canvas = document.createElement('canvas');
     canvas.width  = window.innerWidth;
     canvas.height = window.innerHeight;
+    document.body.appendChild(canvas);
 
     var gl = canvas.getContext('webgl');
 
-    var vsSource = document.getElementById('vs').innerHTML;
-    var fsSource = document.getElementById('fs').innerHTML;
+    var vsSource0 = document.getElementById('brightness-vs').innerHTML;
+    var fsSource0 = document.getElementById('brightness-fs').innerHTML;
 
+    var vsSource1 = document.getElementById('vs').innerHTML;
+    var fsSource1 = document.getElementById('fs').innerHTML;
+
+    var vsSource2 = document.getElementById('bloom-vs').innerHTML;
+    var fsSource2 = document.getElementById('bloom-fs').innerHTML;
 
     function main() {
-        var program = compileShader(gl, vsSource, fsSource);
+        var program0 = compileShader(gl, vsSource0, fsSource0);
+        var program1 = compileShader(gl, vsSource1, fsSource1);
+        var program2 = compileShader(gl, vsSource2, fsSource2);
 
         // position
         {
@@ -23,10 +31,17 @@
                  1.0, -1.0, 0.0
             ];
             var positionVBO = createVBO(gl, positionData);
-            var positionLocation = gl.getAttribLocation(program, 'position');
+
+            var positionLocation0 = gl.getAttribLocation(program0, 'position');
             gl.bindBuffer(gl.ARRAY_BUFFER, positionVBO);
-            gl.enableVertexAttribArray(positionLocation);
-            gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(positionLocation0);
+            gl.vertexAttribPointer(positionLocation0, 3, gl.FLOAT, false, 0, 0);
+
+            var positionLocation1 = gl.getAttribLocation(program1, 'position');
+            gl.bindBuffer(gl.ARRAY_BUFFER, positionVBO);
+            gl.enableVertexAttribArray(positionLocation1);
+            gl.vertexAttribPointer(positionLocation1, 3, gl.FLOAT, false, 0, 0);
+
             gl.bindBuffer(gl.ARRAY_BUFFER, null);
         }
 
@@ -35,13 +50,16 @@
             var colorData = [
                 1.0, 0.0, 0.0, 1.0,
                 0.0, 1.0, 0.0, 1.0,
-                0.0, 0.0, 1.0, 1.0
+                0.0, 0.0, 1.0, 1.0,
+                0.5, 0.5, 0.5, 1.0
             ];
-            var colorLocation = gl.getAttribLocation(program, 'color');
             var colorVBO = createVBO(gl, colorData);
             gl.bindBuffer(gl.ARRAY_BUFFER, colorVBO);
-            gl.enableVertexAttribArray(colorLocation);
-            gl.vertexAttribPointer(colorLocation, 4, gl.FLOAT, false, 0, 0);
+
+            var colorLocation1 = gl.getAttribLocation(program1, 'color');
+            gl.bindBuffer(gl.ARRAY_BUFFER, colorVBO);
+            gl.enableVertexAttribArray(colorLocation1);
+            gl.vertexAttribPointer(colorLocation1, 4, gl.FLOAT, false, 0, 0);
             gl.bindBuffer(gl.ARRAY_BUFFER, null);
         }
 
@@ -53,11 +71,16 @@
                 0.0, 1.0, 
                 1.0, 1.0
             ];
-            var textureCoordLocation = gl.getAttribLocation(program, 'textureCoord');
             var textureCoordVBO = createVBO(gl, textureCoordData);
+
+            var textureCoord0 = gl.getAttribLocation(program0, 'textureCoord');
             gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordVBO);
-            gl.enableVertexAttribArray(textureCoordLocation);
-            gl.vertexAttribPointer(textureCoordLocation, 2, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(textureCoord0);
+            gl.vertexAttribPointer(textureCoord0, 2, gl.FLOAT, false, 0, 0);
+
+            var textureCoord1 = gl.getAttribLocation(program1, 'textureCoord');
+            gl.enableVertexAttribArray(textureCoord1);
+            gl.vertexAttribPointer(textureCoord1, 2, gl.FLOAT, false, 0, 0);
             gl.bindBuffer(gl.ARRAY_BUFFER, null);
         }
 
@@ -72,45 +95,134 @@
         }
 
         // Uniform
-        var matrixLocation  = gl.getUniformLocation(program, 'mvpMatrix');
-        var textureLocation = gl.getUniformLocation(program, 'texture');
+        var matrixLocation  = gl.getUniformLocation(program1, 'mvpMatrix');
+        var textureLocation0 = gl.getUniformLocation(program0, 'texture');
+        var textureLocation1 = gl.getUniformLocation(program1, 'texture');
 
-        var mMatrix = mat4();
-        var vMatrix = mat4();
-        var pMatrix = mat4();
-        var tmpMatrix = mat4();
-        var mvpMatrix = mat4();
+        var minBrightLocation = gl.getUniformLocation(program0, 'minBright');
 
-        vMatrix = mat4.lookAt(vec3( 0.0, 2.0, 5.0 ), vec3(0, 0, 0), vec3(0, 1, 0));
-        pMatrix = mat4.perspective(60, canvas.width / canvas.height, 0.1, 100);
-        tmpMatrix = mat4.multiply(pMatrix, vMatrix);
+        var offsetsLocation = gl.getUniformLocation(program2, 'offsets');
+        var weightsLocation = gl.getUniformLocation(program2, 'weights');
+
+        // Setup matricies
+        {
+            var mMatrix = mat4();
+            var vMatrix = mat4();
+            var pMatrix = mat4();
+            var tmpMatrix = mat4();
+            var mvpMatrix = mat4();
+
+            vMatrix = mat4.lookAt(vec3( 0.0, 0.0, 5.0 ), vec3(0, 0, 0), vec3(0, 1, 0));
+            pMatrix = mat4.perspective(60, canvas.width / canvas.height, 0.1, 100);
+            tmpMatrix = mat4.multiply(pMatrix, vMatrix);
+        }
 
         gl.enable(gl.DEPTH_TEST);
         gl.depthFunc(gl.LEQUAL);
 
 
+        // var offscreen = createFramebuffer(gl, window.innerWidth, window.innerHeight);
+
+        var texture;
+        var rad = 10 * Math.PI / 180;
+
+        var captureScreen = gl.createTexture();
+        gl.activeTexture(gl.TEXTURE1);
+        gl.bindTexture(gl.TEXTURE_2D, captureScreen);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, window.innerWidth, window.innerHeight, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+        // Sampling
+        var SAMPLE_COUNT = 4;
+        for (var j = 0; j < 2; j++) {
+            var offset = new Array(SAMPLE_COUNT);
+            var weight = new Array(SAMPLE_COUNT);
+            var total = 0;
+
+            for (var i = 0; i < SAMPLE_COUNT; i++) {
+                var p = (i - (SAMPLE_COUNT - 1) * 0.5) * 0.01;
+                offset[i] = (j % 2 === 0) ? [p, 0, 0, 0] : [0, p, 0, 0];
+                weight[i] = Math.exp(-p * p / 2) / Math.sqrt(Math.PI * 2);
+                total += weight[i];
+            }
+            for (var i = 0; i < SAMPLE_COUNT; i++) {
+                weight[i] /= total;
+            }
+        }
+
+        function runLoop() {
+            // Pass
+            {
+                // メインシーンをレンダリング
+
+                // メインシーン用プログラムを使用
+                gl.useProgram(program1);
+
+                // デバイス用のバッファにレンダリングするためframebufferをnullに
+                gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+                // クリアを実行
+                gl.clearColor(0.3, 0.0, 0.0, 1.0);
+                gl.clearDepth(1.0);
+                gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+                // モデルの状態をアップデート
+                // mat4.rotate(mMatrix, rad, vec3(0, 1, 0), mMatrix);
+                mat4.multiply(tmpMatrix, mMatrix, mvpMatrix);
+
+                // uniform変数にデータをアップロード
+                gl.uniformMatrix4fv(matrixLocation, false, mvpMatrix);;
+
+                // 0番のテクスチャをアクティブに
+                gl.activeTexture(gl.TEXTURE0);
+                gl.bindTexture(gl.TEXTURE_2D, texture);
+                gl.uniform1i(textureLocation1, 0);
+
+                // メインシーンをdraw
+                gl.drawElements(gl.TRIANGLES, indexData.length, gl.UNSIGNED_SHORT, 0);
+            }
+
+            // Pass
+            {
+
+                // 現在のバッファの状態をコピー
+                // 1番のテクスチャにコピーする（上記でテクスチャ使い終わっているから要らない処理）
+                gl.activeTexture(gl.TEXTURE1);
+                gl.bindTexture(gl.TEXTURE_2D, captureScreen);
+                gl.copyTexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 0, 0, window.innerWidth, window.innerHeight, 0);
+
+                gl.useProgram(program0);
+                // gl.bindFramebuffer(gl.FRAMEBUFFER, offscreen.framebuffer);
+
+                gl.clearColor(0.0, 0.0, 0.0, 1.0);
+                gl.clearDepth(1.0);
+                gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+                gl.uniform1i(textureLocation0, 1);
+                gl.uniform1f(minBrightLocation, 0.5);
+                // gl.uniform1fv(arrayTestLocation, new Float32Array([0.5, 0.1, 0.3, 0.4, 1.0, 0.0]));
+
+                gl.drawElements(gl.TRIANGLES, indexData.length, gl.UNSIGNED_SHORT, 0);
+            }
+
+
+            gl.flush();
+
+            requestAnimationFrame(runLoop);
+        }
+
+
+        // Load a texture.
         var img = new Image();
         img.onload = function() {
             gl.activeTexture(gl.TEXTURE0);
-            var texture = createTexture(gl, this);
+            texture = createTexture(gl, this);
             runLoop();
         };
         img.src = 'img/texture.png';
-
-        var count = 0;
-        function runLoop() {
-            gl.clearColor(0.0, 0.0, 0.0, 1.0);
-            gl.clearDepth(1.0);
-            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-            count += 0.5;
-            var rad = (count % 360) * Math.PI / 180;
-
-            // gl.uniformMatrix4fv();
-            gl.drawElements(gl.TRIANGLES, indexData.length, gl.UNSIGNED_SHORT, 0);
-
-            gl.flush();
-        }
     }
 
     main();
